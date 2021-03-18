3188b5b255ca9d0cb43706b0c1663016ffbb5741
jamestang12 <homing.tang@ryerson.ca>
2021-03-18 00:54:29 -0400

    Create User Route Status Update & Get User By Username Bug Fixed

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -15,6 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime; 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 
 
@@ -28,26 +32,32 @@ public class UserController{
     @Autowired
     public UserRepository userRepository;
 
-    // @GetMapping(value="/user/{id}")
-    // public User getUserById(@PathVariable("id") String id){
-    //     User user = userRepository.findBy(id)
-    //     User returnValue = new User(user.getId(), "", user.getEmail(), user.getId(), user.getDate());
-    //     return returnValue;
-    // }
+    @GetMapping(value="/user/{username}")
+    public User getUserById(@PathVariable("username") String username){
+        User user = userRepository.findByUsername(username);
+        User returnValue = new User(user.getId(), "", user.getEmail(), user.getId(), user.getDate());
+        return returnValue;
+    }
 
     @PostMapping(value="/createUser")
-    public String createUser(@RequestBody User user){
+    @ResponseBody
+    public ResponseEntity createUser(@RequestBody User user){
         String id = UUID.randomUUID().toString();
         user.setId(id);
         user.setDate(LocalDateTime.now());
 
+        if(userRepository.findByUsername(user.getUsername()) != null ||  userRepository.findByEmail(user.getEmail()) != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
+        }
      
         String encryptedString = AES.encrypt(user.getPassowrd(), secretKey) ;
         user.setPassword(encryptedString);
         User insertedUser = userRepository.insert(user);
 
 
-        return "User created";
+        // return "User created";
+        return new ResponseEntity<>("User created",HttpStatus.OK);
+
     }
 
 
--- src/main/java/com/example/demo/dao/UserRepository.java
+++ src/main/java/com/example/demo/dao/UserRepository.java
@@ -6,12 +6,13 @@ import org.springframework.data.mongodb.repository.Query;
 
 
 
+
 import com.example.demo.model.User;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String>{
+public interface UserRepository extends MongoRepository<User, String> {
     
-    User findBy(String id);
-     
+    User findByUsername(String username);
+    User findByEmail(String email);
 
 }
